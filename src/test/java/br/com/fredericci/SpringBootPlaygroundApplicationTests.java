9d121eacbc9b2c708577a7f047d35d026de48bd1
Dennys Fredericci <dennys.fredericci@kbc.be>
2019-11-15 13:18:49 +0100

    init

---
+++ src/test/java/br/com/fredericci/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fredericci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
